063cc5135876ec876ac05012290baeca83ff5e06
Gabriela Bés <gabriela.ifrs.ads@gmail.com>
2025-07-30 11:07:45 -0300

    Update vogais e consoantes.c

--- vogais e consoantes.c
+++ vogais e consoantes.c
@@ -2,12 +2,13 @@
 #include<string.h>
 
 int main (){
-    char palavra[50];
-    int comprimento, vogais = 0, consoantes = 0;
+    char palavra[50], palavra_inversa[50];
+    int comprimento = 0, vogais = 0, consoantes = 0;
     printf("Digite uma palavra: ");
     fgets(palavra, sizeof(palavra), stdin);
+    palavra[strcspn(palavra, "\n")] = '\0'; //remoção do caracter \0//
     comprimento=strlen(palavra);
-    printf("%d\n", comprimento);
+    printf("A palavra digitada tem %d letras.\n", comprimento);
     for( int i=0; i<comprimento; i++){
         if( palavra[i]== 'a' || palavra[i]=='e' || palavra[i]=='i' || palavra[i]=='o' || palavra[i]=='u' ){
             vogais++;
@@ -15,6 +16,20 @@ int main (){
                 consoantes++;
             }
     }
-    printf(" %d", consoantes);
-    printf(" %d", vogais);
+    printf("Número de consoantes: %d", consoantes);
+    printf("\nNúmero de vogais: %d\n", vogais);
+    
+    int j=0;
+    printf("Palavra invertida: ");
+    for (int i= comprimento-1; i>=0; i--){
+        printf("%c", palavra[i]);
+        palavra_inversa[j]=palavra[i];
+        j++;
+    }
+    palavra_inversa[j] = '\0';
+    if (strcmp(palavra, palavra_inversa) == 0) {
+        printf("\nÉ palíndromo!\n");
+    } else {
+        printf("\nNão é palíndromo!\n");
+    }
 }
